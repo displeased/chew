433bc2dc00c8f7dfdd4b55322e64e25484877678
Mickey Polito <mwpolito1@gmail.com>
2023-09-21 16:26:04 -0400

    test: corrected tests to better reflect at_line functionality

--- tests/test_primitive.py
+++ tests/test_primitive.py
@@ -6,6 +6,9 @@ import unittest
 from chew.primitive import *
 
 TEST_STRING = "Hello World!\nI love Snails!\nThat is All."
+ONE_LINES = "\nI love Snails!\nThat is All."
+TWO_LINES = "\nThat is All."
+THREE_LINES = ""
 
 
 class TestPrimitive(unittest.TestCase):
@@ -16,7 +19,13 @@ class TestPrimitive(unittest.TestCase):
         self.assertEqual(next_item(""), None)
 
     def test_at_line(self):
-        self.assertEqual(at_line(TEST_STRING, "Hello World!\nI love Snails!"), 1)
+        self.assertEqual(at_line(TEST_STRING, ONE_LINES), 0)
+
+    def test_at_line_two_lines(self):
+        self.assertEqual(at_line(TEST_STRING, TWO_LINES), 1)
+
+    def test_at_line_three_lines(self):
+        self.assertEqual(at_line(TEST_STRING, THREE_LINES), 2)
 
     def test_at_line_on_exhausted(self):
         self.assertEqual(at_line("", ""), 0)
